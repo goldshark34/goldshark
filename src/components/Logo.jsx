59b7f554fd177dfabab4300444929daaf73190c9
Gold Shark Yachting <goldsharkyachting.34@gmail.com>
2026-01-16 22:17:23 +0300

    update: Logo URL güncellendi
    - Yeni logo URL'si: https://img.sanishtech.com/u/899c1a0738c3b7bac71f9bd547d661ad.jpg
    - Logo bileşeninde sadece URL değiştirildi
    - Diğer tüm özellikler korundu

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = ({ width = 70, height = 70, className = "", style = {} }) => {
   // Yeni logo URL'si
-  const logoUrl = "https://img.sanishtech.com/u/d8f847772a55382e7fba8bb8b16ac21a.png"
+  const logoUrl = "https://img.sanishtech.com/u/899c1a0738c3b7bac71f9bd547d661ad.jpg"
   
   return (
     <img 
